7da851eddeb19abcacb761cf4aa6f912f8c89d32
GordonChanFZ <77518307+GordonChanFZ@users.noreply.github.com>
2023-06-26 05:32:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ import (
 
 	"fmt"
 
-	"github.com/xueqiu/rdr/decoder"
-	"github.com/xueqiu/rdr/dump"
+	"github.com/GordonChanFZ/rdr/decoder"
+	"github.com/GordonChanFZ/rdr/dump"
 )
 
 //go:generate go-bindata -prefix "static/" -o=static/static.go -pkg=static -ignore static.go static/...
